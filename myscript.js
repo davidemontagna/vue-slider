712bf9b8b03799854b8b8e87ad8e969dd74c2976
davidemontagna <davidemontagna92@gmail.com>
2022-01-14 14:35:44 +0100

    inserisco solo un'immagine e nascondo le altre

--- myscript.js
+++ myscript.js
@@ -1,4 +1,51 @@
-const items = [
+/*Descrizione:
+Partendo dal markup della versione svolta in js plain, rifare lo slider
+ma questa volta usando Vue.
+
+Bonus:
+1- al click su una thumb, visualizzare in grande l’immagine corrispondente
+2- applicare l’autoplay allo slider: ogni 3 secondi, cambia immagine
+   automaticamente
+3- quando il mouse va in hover sullo slider, bloccare l’autoplay e
+   farlo riprendere quando esce  */
+
+let app = new Vue({
+    el: "#app",
+    data: {
+        images: [
+            'img/01.jpg',
+            'img/02.jpg',
+            'img/03.jpg',
+            'img/04.jpg',
+            'img/05.jpg'
+        ],
+        title: [
+            'Svezia',
+            'Svizzera',
+            'Gran Bretagna',
+            'Germania',
+            'Paradise'
+        ],
+        text: [
+            'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+            'Lorem ipsum',
+            'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+            'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+            'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+        ],
+        activeOn: 0
+    },
+    methods: {
+        imgActive: function(indexImg){
+            if(indexImg == this.activeOn){
+                return "active";
+            }
+            return "image";
+        }
+    }
+})
+
+/*const items = [
     'img/01.jpg',
     'img/02.jpg',
     'img/03.jpg',
@@ -20,12 +67,12 @@ const text = [
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
     'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-];
+];*/
 
 
 
 //Inserimento immagine grande e scritte
-let bigItem = '';
+/*let bigItem = '';
 let elementActive = 0;
 
 for(let index=0; index < items.length; index++){
@@ -83,7 +130,7 @@ const down = document.querySelector('.dm-arrow-down');
 
 /*click freccia GIU' rimuovo una classe dall'immagine precedente e la aggiungo alla successiva
 sia per l'immagine grande che per quelle piccole*/
-down.addEventListener('click', function(){
+/*down.addEventListener('click', function(){
     elementActive++;
     if(elementActive > 4){
         elementActive = 0;
@@ -99,7 +146,7 @@ down.addEventListener('click', function(){
 
 /*click freccia SU rimuovo una classe dall'immagine precedente e la aggiungo alla successiva
 sia per l'immagine grande che per quelle piccole*/
-up.addEventListener('click', function(){
+/*up.addEventListener('click', function(){
     elementActive--;
     if(elementActive < 0){
         elementActive = 4;
@@ -110,4 +157,4 @@ up.addEventListener('click', function(){
     smImageActive.classList.remove("dm-sm-opacity-on");
     smImageActive = document.getElementById("thumbnailsitem-"+elementActive);
     smImageActive.classList.add("dm-sm-opacity-on");
-});
+});*/
